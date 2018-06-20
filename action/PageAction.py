9a483c921a96f01643df52210557b72428c62d4c
wangsikun <wangsikun@cnbmtech.com>
2018-06-20 11:18:56 +0800

    [debug]修改函数loadPage的sleep时间

--- action/PageAction.py
+++ action/PageAction.py
@@ -377,9 +377,9 @@ def press_key(mykey,*arg):        #模拟单按键，如： "tab"、"enter"
 def loadPage(*arg):     # 设置页面加载时间
     global driver
     try:
-        sleep(1)
+        sleep(0.5)
         driver.set_page_load_timeout(10)
-        sleep(1)
+        sleep(0.5)
     except TimeoutError as e:
         print("********** 等待页面加载超时 **********")
         raise TimeoutError(e)
